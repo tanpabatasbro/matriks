ce7ca4e93ef5ff612b0d9b126efcd0e514225469
tanpabatasbro <115479649+tanpabatasbro@users.noreply.github.com>
2022-11-13 20:48:38 +0700

    Add files via upload

---
+++ matriks.py
@@ -0,0 +1,16 @@
+matriks_a = [[1,0],[0,1]]
+print(matriks_a)
+
+matriks_b = [[1,0,1],[0,1,0],[1,0,1]]
+print(matriks_b)
+
+matriks_c = [[1,0,1,0],[0,1,0,1],[1,0,0,1],[0,1,1,0]]
+print(matriks_c)
+print(20*"=")
+m = 4
+n = 5
+x =  [0]*m
+
+for i in range(m):
+    x[i] = [1]*n
+print(x)
